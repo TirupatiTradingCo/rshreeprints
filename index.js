a229ef53bd9b347ab38f13c9b41d2ebbc2e300a4
TirupatiTradingCo <tirupatitradingcompany96@gmail.com>
2024-06-16 00:07:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
             formObject[key] = value;
         });
 
-        fetch('https://rshreeprints-dda0bdd19954.herokuapp.com//contact', {
+        fetch('https://rshreeprints-dda0bdd19954.herokuapp.com/contact', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
